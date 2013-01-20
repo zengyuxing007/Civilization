b9eda325d385b0f30126ba90826d7b3cdce5f9a5
Steven Norwood <steven.n119@gmail.com>
2013-01-19 19:28:39 -0500

    Working tech_cards.cpp

--- tech_cards.cpp
+++ tech_cards.cpp
@@ -1,32 +1,42 @@
 // Types of tickets
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 class tech_card
 {
 	private:
 		int level;
-		Background background();
-		Icon icon();
-		String ability;
+		//Background background();
+		//Icon icon();
+		string ability;
 	public:
 		tech_card()
 		{
 			level = 0;
-			ability = none;
+			ability = "none";
 		}
 		
-		tech_card(int l, Icon i, String a)
+		tech_card(int l, /*Icon i,*/ string a)
 		{
 			level = l;
-			icon = i;
+			// icon = i;
 			ability = a;
 		}
-		tech_card(int l, Icon i)
+		tech_card(int l/*,*/ /*Icon i*/)
 		{
 			level = l;
-			icon = i;
+			// icon = i;
 		}
 
-		pickup(Player p)
+		void pickup(/*Player p*/)
 		{
-			p.tech_tree.add(level);
+			//p.tech_tree.add(level);
 		}
+};
+
+int main()
+{
+	cout << "Hello World";
+	return 0;
 }
