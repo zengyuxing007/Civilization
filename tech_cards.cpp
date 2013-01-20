419ed29ebbc5fc731b3666f8b237db4a817010a7
Steven Norwood <steven.n119@gmail.com>
2013-01-19 20:46:36 -0500

    Added Horseback Riding and Player classes

--- tech_cards.cpp
+++ tech_cards.cpp
@@ -3,37 +3,91 @@
 #include <cstdlib>
 #include <iostream>
 using namespace std;
+
+//Base template for tech cards
 class tech_card
 {
 	private:
 		int level;
 		//Background background();
+		//Player owner();
 		//Icon icon();
 		string ability;
 	public:
+		//Default Constructor
 		tech_card()
 		{
 			level = 0;
 			ability = "none";
 		}
 		
-		tech_card(int l, /*Icon i,*/ string a)
+		//Constructor
+		tech_card(int l, /*, Player o, Icon i,*/string a)
 		{
+			// owner = o;
 			level = l;
 			// icon = i;
 			ability = a;
 		}
-		tech_card(int l/*,*/ /*Icon i*/)
-		{
-			level = l;
-			// icon = i;
-		}
 
+		//Function to play on pickup
 		void pickup(/*Player p*/)
 		{
 			//p.tech_tree.add(level);
 		}
 };
+
+//Horseback Riding
+class horseback_riding:tech_card
+{
+	public:
+
+		//Play on pickup
+		void pickup(/*Player p*/)
+		{/*
+			p.tech_tree.add(level);
+			if (p.move_speed < 3)
+				p.move_speed = 3;
+		*/}
+
+		void ability()
+		{
+			if (true/*owner.resources.getSilk() >= 1*/)
+			{/*
+				Player p = choosePlayer();
+				owner.trade.add(6);
+				p.trade.add(9);
+			*/}
+		}
+};
+
+class Player
+{
+	private:
+		int gold;
+		int trade;
+		int move_speed;
+		int city_limit;
+	public:
+		Player()
+		{
+			gold = 0;
+			trade = 0;
+			move_speed = 2;
+			city_limit = 2;
+		}
+
+		int getGold()
+		{
+			return gold;
+		}
+
+		int getTrade()
+		{
+			return trade;		
+		}
+};
+
 
 int main()
 {
