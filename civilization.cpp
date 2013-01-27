00627a7af647c84bdad14273425f89078f61c34e
Steven Norwood <steven.n119@gmail.com>
2013-01-26 20:44:51 -0500

    civilization class

---
+++ civilization.cpp
@@ -0,0 +1,13 @@
+//Contains all information pertaining to a player
+#include "Player.h"
+#include "resource_bank.h"
+#include "attributes.h"
+#include "tech_tree.h"
+
+using namespace std;
+
+//Constructor
+Player::Player()
+{	
+	//techs = new tech_tree (*this);
+}
